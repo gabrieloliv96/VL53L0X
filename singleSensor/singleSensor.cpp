e10253fca8df5f416b32e15ae1bd067999513803
gabrieloliv96 <60796525+gabrieloliv96@users.noreply.github.com>
2020-02-26 20:53:36 -0300

    Update singleSensor.cpp

--- singleSensor/singleSensor.cpp
+++ singleSensor/singleSensor.cpp
@@ -1,28 +1,25 @@
+/*
+ *   Q0000
+ *   AUTOR:   Gabriel Oliveira
+ *   SKETCH:  Módulo para 
+ *   DATA:    26/02/2020
+*/
+
 #include "mbed.h"
 #include "VL53L0X.h"
  
-//DigitalOut pp(PA_1);
-//Serial pc(PB_10,PB_11);
-//I2C i2c(PB_9,PB_8);
- 
-//VL53L0X sensor(PB_9,PB_8);
+
 uint16_t dist;
 int main() {
-    //float dist;
-    //wait(1);
-    //pc.baud(9600);
-    //pc.printf("Chegou aqui\r\n");
-    printf("Chegou aqui\r\n");
+    // Criando o objeto para chamada do sensor
     VL53L0X* sensor = new VL53L0X(PB_9,PB_8);
+    // Ligando
     sensor->init(true);
     sensor->startContinuous();
-    //pc.printf("db1\r\n");
-    printf("db1\r\n");
+    
     while(1){
-        //pc.printf("db2\r\n");
-        printf("db2\r\n");
+        // Iniciando a medição
         dist=sensor->readRangeContinuousMillimeters();
-        //pc.printf("%d\r\n",dist);
         printf("%d\r\n",dist);
         wait(1);
     }
