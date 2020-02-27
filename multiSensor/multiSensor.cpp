995d5e25d8b22a4975cca6f87ac41a39fe3ada6f
gabrieloliv96 <60796525+gabrieloliv96@users.noreply.github.com>
2020-02-26 21:10:11 -0300

    Update multiSensor.cpp

--- multiSensor/multiSensor.cpp
+++ multiSensor/multiSensor.cpp
@@ -1,16 +1,23 @@
+/*
+ *   Q0000
+ *   AUTOR:   Gabriel Oliveira
+ *   SKETCH:  Módulo do sensor multiplo VL53L0X para BluePill(Nucleo STM32F103)
+ *   DATA:    26/02/2020
+*/
+
 #include "mbed.h"
 #include "VL53L0X.h"
-//Multi-sensor 
+// Endereços 
 #define range1_addr (0x56)
 #define range2_addr (0x60)
 #define range3_addr (0x64)
 #define range4_addr (0x68)
-//Preciso dar mesmo um pino de reset pra cada sensor?
+// Pinos de reset
 #define range1_XSHUT   PA_2
 #define range2_XSHUT   PA_3
 #define range3_XSHUT   PA_4
 #define range4_XSHUT   PA_5
-
+// Pinos de dados
 #define VL53L0_I2C_SDA   PB_9
 #define VL53L0_I2C_SCL   PB_8  
 #define SERIAL_TX PB_10
@@ -24,10 +31,9 @@ static DevI2C devI2c(VL53L0_I2C_SDA,VL53L0_I2C_SCL);
 int main()
 {   
     pc.baud(9600);
-    //Debug1
-    //pc.printf("Debug1\r\n");
-    /*Contruct the sensors*/
-    //O pino desliga pra receber o endereço 
+    
+    // Contrutores 
+    // O pino desliga para receber o endereço
     //VL53L0X -> Recebendo SDA/SLC,pino de reset, Não sei o que é NC
     static DigitalOut shutdown1_pin(range1_XSHUT);
     static VL53L0X range1(&devI2c, &shutdown1_pin, NC);
@@ -38,38 +44,34 @@ int main()
     static DigitalOut shutdown4_pin(range4_XSHUT);
     static VL53L0X range4(&devI2c, &shutdown4_pin, NC);
     
-    /*Initial all sensors*/   
-    //O endereço fica salvo? Precisa teste
+    // Ligando os sensores com novo endereço  
     range1.init_sensor(range1_addr);
     range2.init_sensor(range2_addr);
     range3.init_sensor(range3_addr);
     range4.init_sensor(range4_addr);
-    //printf("AddrDebug: %hhX", range1_addr);
-    //printf("AddrDebug: %hhX", range2_addr);
-    /*Get datas*/
+   
+    // Variaveis de dados 
     uint32_t distance1;
     uint32_t distance2;
     uint32_t distance3;
     uint32_t distance4;
-    //Status qualify
+ 
+    // Error NULL
     int status1;
     int status2;
     int status3;
     int status4;
-    //Debug2
-    //pc.printf("Debug2\r\n");
+    
     
     while(1){
-        //Debug3
-        //pc.printf("Debug3-laco\r\n");
+        
         status1 = range1.get_distance(&distance1);
         if (status1 == VL53L0X_ERROR_NONE) {
             pc.printf("Range1 [mm]:            %6ld\r\n", distance1);
         } else {
             pc.printf("Range1 [mm]:                --\r\n");
         }
-        //Debug4
-        //pc.printf("Debug4-laco\r\n");
+        
         status2 = range2.get_distance(&distance2);
         if (status2 == VL53L0X_ERROR_NONE) {
             pc.printf("Range2 [mm]:            %6ld\r\n", distance2);
@@ -88,8 +90,7 @@ int main()
         } else {
             pc.printf("Range2 [mm]:                --\r\n");
         }
-        //Debug5
-        //pc.printf("Debug5-EOF\r\n");
+        
         wait(0.5);                 
     }
  
